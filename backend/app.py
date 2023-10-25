2db1ec0f7e137920498764f7cc7159add091b824
hebounds <hayeservin@gmail.com>
2023-10-25 10:09:18 -0400

    App name changes and timings generator

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,49 @@
 from flask import Flask, send_file
 from flask_cors import CORS
 
-from bokeh.plotting import figure
-from bokeh.embed import json_item
-from bokeh.io import export_png, export_svgs
+import plotly.graph_objects as go
 
-from numpy import cos, linspace
+import pandas as pd
+import numpy as np
+
+import time
+import datetime
+
+# If true writes graph generation timing to "timings.txt"
+timingBool = True
+
+np.random.seed(1)
+
 
 app = Flask(__name__)
 # CORS enabled so react frontend can pull data from python backend
 CORS(app)
 
 @app.route('/plot1', methods=["GET"])
-def plot1(): 
-    x = linspace(-6, 6, 100)
-    y = cos(x)
-    p = figure(width=500, height=500, toolbar_location=None, title="Plot 1")
-    p.circle(x, y, size=7, color="firebrick", alpha=0.5)
+def plot1():
+    time1 = time.perf_counter() 
+    N = 100
+    x = np.random.rand(N)
+    y = np.random.rand(N)
+    colors = np.random.rand(N)
+    sz = np.random.rand(N) * 30
 
-    export_png(p, filename = "bokeh_plot.png")
-    return send_file("bokeh_plot.png", mimetype='image/png')
-    # export_svgs(p, filename = "bokeh_plot.svg")
-    # return send_file("bokeh_plot.svg", mimetype='image/svg+xml')
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(
+        x=x,
+        y=y,
+        mode="markers",
+        marker=go.scatter.Marker(
+            size=sz,
+            color=colors,
+            opacity=0.6,
+            colorscale="Viridis"
+        )
+    ))
+    fig.write_image("plotly_plot.png")
+    time2 = time.perf_counter()
+    if (timingBool):
+        with open('timings.txt', 'a') as timings:
+            curTime = datetime.datetime.now()
+            timings.write(curTime.strftime("%m/%d/%Y, %H:%M:%S") + ": " + str(time2 - time1) + "s\n")
+    return send_file("plotly_plot.png", mimetype='image/png')
